ca246c490ce2913382cf6de9b54c3064a93e980e
thinkofher <mail@bdudek.xyz>
2021-09-07 23:20:43 +0200

    Add documentation to api package.

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -1,3 +1,13 @@
+/*
+Package api implements http REST API compatible with xBrowserSync service.
+
+Below handlers contains information about what method should they be used with
+and what path should they be mounted to. It is dictated by xBrowserSync original
+API implementation.
+
+See xBrowserSync documentation for further information about what it is and how it
+works.
+*/
 package api
 
 import (
@@ -9,10 +19,33 @@ import (
 
 	"github.com/alioygur/gores"
 	"github.com/go-chi/chi/v5"
+
 	"github.com/thinkofher/lalyta/pkg/models"
 	"github.com/thinkofher/lalyta/pkg/service/gen"
 )
 
+// Info retrieves information describing the xBrowserSync service.
+//
+//  GET /info
+//
+// Response example:
+//
+//  {
+//    "maxSyncSize": 204800,
+//    "message": "",
+//    "status": 1,
+//    "version": "1.1.13"
+//  }
+//
+// * Status ("status", int): current service status code. 1 = Online; 2 = Offline;
+// 3 = Not accepting new syncs.
+//
+// * Message ("message", string): service information message.
+//
+// * Version ("version", string): API version service is using.
+//
+// * Maximum sync size ("maxSyncSize", int): maximum sync size (in bytes)
+// allowed by the service.
 func Info(location, msg, version string) http.HandlerFunc {
 	type response struct {
 		MaxSyncSize int64  `json:"maxSyncSize"`
@@ -32,13 +65,43 @@ func Info(location, msg, version string) http.HandlerFunc {
 
 const idLength = 32
 
+// BookmarksStorage describes methods required for storing and retrieving
+// bookmarks from data source. It has to be thread safe.
 type BookmarksStorage interface {
 	SetBookmarks(ctx context.Context, b models.Bookmarks) error
 	GetBookmarks(ctx context.Context, id string) (*models.Bookmarks, error)
 }
 
+// ErrBookmarksNotFound is returned by BookmarksStorage when
+// there is no bookmarks with given ID in storage.
 var ErrBookmarksNotFound = errors.New("bookmarks with given id has been not found")
 
+// CreateBookmarks creates a new (empty) bookmark sync and returns
+// the corresponding ID.
+//
+//  POST /bookmarks
+//
+// Post body example:
+//
+//  {
+//    "version": "1.0.0"
+//  }
+//
+// Response example:
+//
+//  {
+//    "id": "52758cb942814faa9ab255208025ae59",
+//    "lastUpdated": "2016-07-06T12:43:16.866Z",
+//    "version": "1.0.0"
+//  }
+//
+// * ID ("id", string): 32 character alphanumeric sync ID.
+//
+// * Last updated ("lastUpdated", timestamp as string): last updated timestamp
+// for created bookmarks.
+//
+// * Version ("version", version): version number of the xBrowserSync client
+// used to create the sync.
 func CreateBookmarks(storage BookmarksStorage) http.HandlerFunc {
 	type payload struct {
 		Version string `json:"version"`
@@ -85,6 +148,31 @@ func CreateBookmarks(storage BookmarksStorage) http.HandlerFunc {
 	})
 }
 
+// Bookmarks retrieves the bookmark sync corresponding to the
+// provided sync ID.
+//
+//  GET /bookmarks/{id}
+//
+// Query params:
+//
+// * id: 32 character alphanumeric sync ID.
+//
+// Response example:
+//
+//  {
+//    "bookmarks": "DWCx6wR9ggPqPRrhU4O4oLN5P09oULX4Xt+ckxswtFNds...",
+//    "lastUpdated": "2016-07-06T12:43:16.866Z",
+//    "version": "1.0.0"
+//  }
+//
+// * Bookmarks ("bookmarks", string): encrypted bookmark data salted using
+// secret value.
+//
+// * Last updated: ("lastUpdated", timestamp as string): last updated timestamp
+// for retrieved bookmarks.
+//
+// * Version ("version", string): version number of the xBrowserSync client used
+// to create the sync.
 func Bookmarks(storage BookmarksStorage) http.HandlerFunc {
 	type response struct {
 		Bookmarks   string    `json:"bookmarks"`
@@ -115,6 +203,36 @@ func Bookmarks(storage BookmarksStorage) http.HandlerFunc {
 	})
 }
 
+// UpdateBookmarks updates the bookmark sync data corresponding to the
+// provided sync ID with the provided encrypted bookmarks data.
+//
+//  PUT /bookmarks/{id}
+//
+// Query params:
+//
+// * id: 32 character alphanumeric sync ID.
+//
+// Post body example:
+//
+//  {
+//    "bookmarks": "DWCx6wR9ggPqPRrhU4O4oLN5P09oULX4Xt+ckxswtFNds...",
+//    "lastUpdated": "2016-07-06T12:43:16.866Z",
+//  }
+//
+// * Bookmarks ("bookmarks", string): encrypted bookmark data salted using
+// secret value.
+//
+// * Last updated ("lastUpdated", timestamp as string): last updated timestamp
+// to check against existing bookmarks.
+//
+// Response example:
+//
+//  {
+//    "lastUpdated": "2016-07-06T12:43:16.866Z"
+//  }
+//
+// Last updated ("lastUpdated", timestamp as string): last updated timestamp
+// for updated bookmarks.
 func UpdateBookmarks(storage BookmarksStorage) http.HandlerFunc {
 	type payload struct {
 		Bookmarks   string    `json:"bookmarks"`
@@ -170,6 +288,23 @@ func UpdateBookmarks(storage BookmarksStorage) http.HandlerFunc {
 	})
 }
 
+// LastUpdated retrieves the bookmark sync last updated timestamp
+// corresponding to the provided sync ID.
+//
+//   GET /bookmarks/{id}/lastUpdated
+//
+// Query params:
+//
+// * id: 32 character alphanumeric sync ID.
+//
+// Response example:
+//
+//  {
+//    "lastUpdated":"2016-07-06T12:43:16.866Z"
+//  }
+//
+// * Last updated ("lastUpdated", timestamp as string): last updated
+// timestamp for corresponding bookmarks.
 func LastUpdated(storage BookmarksStorage) http.HandlerFunc {
 	type response struct {
 		LastUpdated time.Time `json:"lastUpdated"`
@@ -196,6 +331,23 @@ func LastUpdated(storage BookmarksStorage) http.HandlerFunc {
 	})
 }
 
+// Version retrieves the bookmark sync version number of the xBrowserSync client
+// used to create the bookmarks sync corresponding to the provided sync ID.
+//
+//  GET /bookmarks/{id}/version
+//
+// Query params:
+//
+// * id: 32 character alphanumeric sync ID.
+//
+// Response example:
+//
+//  {
+//    "version":"1.0.0"
+//  }
+//
+// Version ("version", string): version number of the xBrowserSync client
+// used to create the sync.
 func Version(storage BookmarksStorage) http.HandlerFunc {
 	type response struct {
 		Version string `json:"version"`
